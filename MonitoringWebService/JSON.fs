399bd27b88963c3fb4619d4caa32851af0333c6f
Max Lorenz <max-lorenz@gmx.net>
2015-05-15 15:48:00 +0200

    Uploading existing code

---
+++ MonitoringWebService/JSON.fs
@@ -0,0 +1,22 @@
+﻿module JSON
+
+type Json = 
+    | JString of string
+    | JNumber of double
+    | JObject of (string * Json) list
+    | JArray of Json list
+    | JBool of bool
+    | JNull
+
+let concatList = String.concat ", "
+let keyValue convert (name, value) = 
+    sprintf "\"%s\": %s" name (convert value)
+
+let rec toString (json: Json): string = 
+    match json with
+        | JString a -> sprintf "\"%s\"" a
+        | JNumber a -> sprintf "%g" a
+        | JObject a -> concatList (List.map (keyValue toString) a) |> sprintf "{ %s }"
+        | JArray a -> concatList (List.map toString a) |> sprintf "[ %s ]"
+        | JBool a -> if a then "true" else "false"
+        | JNull -> "null"
---
+++ MonitoringWebService/Webserver.fs
@@ -0,0 +1,37 @@
+﻿module Webserver
+
+open System.Net
+open System.Text
+open System.IO
+
+let listen (local: bool) (port: int) (getResponse: string -> string) =
+
+    let host = sprintf "http://%s:%i/" (if local then "localhost" else "*") port 
+
+    let asBytes (text: string) = Encoding.UTF8.GetBytes(text)
+    let listener = new HttpListener()
+
+    listener.Prefixes.Add(host)
+    listener.Start()
+
+    while true do
+
+        let context = listener.GetContext()
+        let request = context.Request
+        let response = context.Response
+
+        // Allow foreign requests
+        response.AppendHeader("Access-Control-Allow-Origin", "*");
+
+        let json = getResponse request.Url.LocalPath |> asBytes
+        let output = response.OutputStream
+
+        response.ContentLength64 <- (int64)json.Length
+
+        output.Write(json, 0, json.Length)
+        output.Close()
+    
+    listener.Stop()
+
+let listenLocal = listen true
+let listenPublic = listen false
