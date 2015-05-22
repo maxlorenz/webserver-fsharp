b18a104b46cb8ade5fc6ac333ed08edd783f551c
Max Lorenz <max-lorenz@gmx.net>
2015-05-22 16:34:58 +0200

    Implemented a WMI Module that executes WQL Queries and returns the results as a sequence

---
+++ MonitoringWebService/WMI.fs
@@ -0,0 +1,19 @@
+﻿module WMI
+
+open System.Management
+
+let getObj (scope: string) query = 
+    let searcher = new ManagementObjectSearcher(scope, query)
+    searcher.Get()
+
+let propertyToTuple (property: PropertyData) = (property.Name, property.Value)
+let moToProperty (mo: ManagementObject) = mo.Properties
+
+let castMap f = Seq.cast >> Seq.map f
+
+let getWQL scope query = 
+    getObj scope query
+        |> castMap moToProperty
+        |> castMap Seq.cast
+        |> (Seq.map >> Seq.map) propertyToTuple
+
