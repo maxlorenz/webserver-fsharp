a21638ee12b931dea6d8a5a7fb6670d748950381
Max Lorenz <max-lorenz@gmx.net>
2015-05-22 16:35:31 +0200

    Changed the server to return WQL queries as JSON

--- MonitoringWebService/Program.fs
+++ MonitoringWebService/Program.fs
@@ -1,22 +1,42 @@
 ﻿open JSON
+open WMI
 open Webserver
-open System.Diagnostics
 
-let cpu = new PerformanceCounter("Processor", "% Processor Time", "_Total")
-let ram = new PerformanceCounter("Memory", "% Committed Bytes In Use", "")
+let rec valueToJSON (value: obj) = 
+    match value with
+    | :? string as s -> 
+        JString (s.Replace("\"", "''").Replace("\\", "\\\\"))
+    | :? bool as b -> 
+        JBool b
+    | :? uint8 | :? uint16 | :? uint32 | :? uint64 -> 
+        JNumber (System.Double.Parse(value.ToString()))
+    | :? (uint8 array) as a -> 
+        JArray [ for element in a do yield valueToJSON element ]
+    | :? (uint16 array) as a -> 
+        JArray [ for element in a do yield valueToJSON element ]
+    | :? (uint32 array) as a -> 
+        JArray [ for element in a do yield valueToJSON element ]
+    | :? (uint64 array) as a -> 
+        JArray [ for element in a do yield valueToJSON element ]
+    | :? (obj array) as a -> 
+        JArray [ for element in a do yield valueToJSON element ]
+    | null -> 
+        JNull
+    | _ -> 
+        JString (value.GetType().FullName)
 
-let getJSON() = 
-    let cpuValue = (double)(cpu.NextValue())
-    let ramValue = (double)(ram.NextValue())
-    JObject [ ("cpu", JNumber cpuValue); ("ram", JNumber ramValue) ] |> JSON.toString
+let objToJSON obj =
+    JObject [ for (name, value) in obj do yield (name, valueToJSON value) ]
+
+let wmiToJSON wmi =
+    JArray [ for obj in wmi do yield objToJSON obj ]
 
 [<EntryPoint>]
 let main argv =
 
-    let route path =
+    let route (path: string) =
         match path with
-        | "/info" -> getJSON()
-        | _ -> "404"
+        | path -> getWQL @"root\cimv2" (sprintf "select * from %s" (path.Split('/').[1])) |> wmiToJSON |> JSON.toString
 
     // 8080 is the port
     // route is function that takes the url and returns some text to send to the client
